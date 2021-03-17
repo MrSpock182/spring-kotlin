c102522d0ae51207c6a74ec9c9f62696d5ba8623
Kleber Nunes <mrspock182@MacBook-Pro-de-Kleber.local>
2021-03-17 13:18:36 -0300

    Aluno vai chegar nesse codigo ao final da aula 1.3

---
+++ src/main/kotlin/br/com/alura/spring/kotlin/resource/HelloResource.kt
@@ -0,0 +1,12 @@
+package br.com.alura.spring.kotlin.resource
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloResource {
+    @GetMapping(value = ["/"])
+    fun hello(): String {
+        return "Hello World!"
+    }
+}
