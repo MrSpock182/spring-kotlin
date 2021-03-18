de5131c0b765a9f206dede1df1b822a62d841e30
Kleber Nunes <mrspock182@MacBook-Pro-de-Kleber.local>
2021-03-18 20:58:58 -0300

    adicionando anotacao repository - final aula 3.1

--- src/main/kotlin/br/com/alura/spring/kotlin/repository/TopicoRepository.kt
+++ src/main/kotlin/br/com/alura/spring/kotlin/repository/TopicoRepository.kt
@@ -2,6 +2,8 @@ package br.com.alura.spring.kotlin.repository
 
 import br.com.alura.spring.kotlin.repository.orm.Topico
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface TopicoRepository : JpaRepository<Topico, Long> {
 }
